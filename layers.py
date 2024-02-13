a950f8b2153ccb3cc90dd8a1aa4ba036aa67bea7
Arielle Rosinski <ariellerosinski0102@gmail.com>
2024-02-13 09:39:52 +0000

    test 1

--- layers.py
+++ layers.py
@@ -158,7 +158,7 @@ class GraphAttentionLayerV2(nn.Module):
         self.alpha = alpha
         self.concat = concat
 
-        self.W = nn.Parameter(torch.empty(size=(in_features, out_features)))
+        self.W = nn.Parameter(torch.empty(size=(in_features, 2*out_features)))
         nn.init.xavier_uniform_(self.W.data, gain=1.414)
         self.a = nn.Parameter(torch.empty(size=(out_features, 1)))
         nn.init.xavier_uniform_(self.a.data, gain=1.414)
@@ -166,14 +166,19 @@ class GraphAttentionLayerV2(nn.Module):
         self.leakyrelu = nn.LeakyReLU(self.alpha)
 
     def forward(self, h, adj):
-        Wh = torch.mm(h, self.W) # h.shape: (N, in_features), Wh.shape: (N, out_features)
-        e = self._prepare_attentional_mechanism_input(Wh)
+        #Wh = torch.mm(h, self.W) # h.shape: (N, in_features), Wh.shape: (N, out_features)
+        Wh1 = torch.matmul(h,self.W[:, :self.out_features]) #N, F @ F , F' --> N, F'
+        Wh2 = torch.matmul(h,self.W[:, self.out_features:])
+        e = Wh1 + Wh2 
+        e = self.leakyrelu(e)
+        e = torch.matmul(e, self.a) #N, F'--> F'
+        #e = self._prepare_attentional_mechanism_input(Wh)
 
         zero_vec = -9e15*torch.ones_like(e)
         attention = torch.where(adj > 0, e, zero_vec)
         attention = F.softmax(attention, dim=1)
         attention = F.dropout(attention, self.dropout, training=self.training)
-        h_prime = torch.matmul(attention, Wh)
+        h_prime = torch.matmul(attention, Wh2)
 
         if self.concat:
             return F.elu(h_prime)
@@ -185,17 +190,66 @@ class GraphAttentionLayerV2(nn.Module):
         # self.a.shape (2 * out_feature, 1)
         # Wh1&2.shape (N, 1)
         # e.shape (N, N)
-        Wh1 = torch.matmul(self.leakyrelu(Wh), self.a[:, :])
-        Wh2 = torch.matmul(self.leakyrelu(Wh), self.a[:, :])
+        Wh1 = torch.matmul(Wh, self.a[:self.out_features, :]) 
+        Wh2 = torch.matmul(Wh, self.a[self.out_features:, :])
         # broadcast add
         e = Wh1 + Wh2.T
-        return e
+        return self.leakyrelu(e)
 
     def __repr__(self):
         return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
     
 
 # class GraphAttentionLayerV2(nn.Module):
+#     """
+#     Simple GAT layer, similar to https://arxiv.org/abs/1710.10903
+#     """
+#     def __init__(self, in_features, out_features, dropout, alpha, concat=True):
+#         super(GraphAttentionLayerV2, self).__init__()
+#         self.dropout = dropout
+#         self.in_features = in_features
+#         self.out_features = out_features
+#         self.alpha = alpha
+#         self.concat = concat
+
+#         self.W = nn.Parameter(torch.empty(size=(in_features, out_features)))
+#         nn.init.xavier_uniform_(self.W.data, gain=1.414)
+#         self.a = nn.Parameter(torch.empty(size=(out_features, 1)))
+#         nn.init.xavier_uniform_(self.a.data, gain=1.414)
+
+#         self.leakyrelu = nn.LeakyReLU(self.alpha)
+
+#     def forward(self, h, adj):
+#         Wh = torch.mm(h, self.W) # h.shape: (N, in_features), Wh.shape: (N, out_features)
+#         e = self._prepare_attentional_mechanism_input(Wh)
+
+#         zero_vec = -9e15*torch.ones_like(e)
+#         attention = torch.where(adj > 0, e, zero_vec)
+#         attention = F.softmax(attention, dim=1)
+#         attention = F.dropout(attention, self.dropout, training=self.training)
+#         h_prime = torch.matmul(attention, Wh)
+
+#         if self.concat:
+#             return F.elu(h_prime)
+#         else:
+#             return h_prime
+
+#     def _prepare_attentional_mechanism_input(self, Wh):
+#         # Wh.shape (N, out_feature)
+#         # self.a.shape (2 * out_feature, 1)
+#         # Wh1&2.shape (N, 1)
+#         # e.shape (N, N)
+#         Wh1 = torch.matmul(self.leakyrelu(Wh), self.a[:, :])
+#         Wh2 = torch.matmul(self.leakyrelu(Wh), self.a[:, :])
+#         # broadcast add
+#         e = Wh1 + Wh2.T
+#         return e
+
+#     def __repr__(self):
+#         return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
+    
+
+# # class GraphAttentionLayerV2(nn.Module):
 #     """
 #     GATv2 layer, vectorized computation without a for loop.
 #     """
