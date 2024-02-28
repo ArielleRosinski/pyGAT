4f7bbac6cc7d2bb9c2d1fcecb831251e24e215c2
Arielle Rosinski <ariellerosinski0102@gmail.com>
2024-02-28 22:13:17 +0000

    citeseer added

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.sparse as sp
 import torch
 import dgl
-from dgl.data import PubmedGraphDataset
+from dgl.data import PubmedGraphDataset, CiteseerGraphDataset
 from dgl import AddSelfLoop
 from dgl import to_networkx
 
@@ -20,13 +20,11 @@ def load_data(path="./data/cora/", dataset="cora"):
     """Load citation network dataset (cora only for now)"""
     print('Loading {} dataset...'.format(dataset))
 
-    if dataset == "cora" or dataset == "citeseer":
+    if dataset == "cora": #or dataset == "citeseer"
         idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
         features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
         labels = encode_onehot(idx_features_labels[:, -1])
 
-    # build graph
-    if dataset == "cora":
         idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
         idx_map = {j: i for i, j in enumerate(idx)}
         edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset), dtype=np.int32)
@@ -36,28 +34,11 @@ def load_data(path="./data/cora/", dataset="cora"):
         idx_train = range(140)
         idx_val = range(200, 500)
         idx_test = range(500, 1500)
-
-    elif dataset == "citeseer":
-        idx = idx_features_labels[:, 0]
-        idx_map = {j: i for i, j in enumerate(idx)}
-        edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset),  dtype=np.dtype(str))
-        edges_temp = np.array(list(map(idx_map.get, edges_unordered.flatten())), dtype=np.dtype(str)).reshape(edges_unordered.shape)
-        edges_temp[edges_temp == 'None'] = '-1'  
-        edges_temp = edges_temp.astype(np.int32)
-        rows_without_none = ~np.any(edges_temp == -1, axis=1)
-        edges = edges_temp[rows_without_none]
-        adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])), shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
-
-        # idx_train = range(120)
-        # idx_val = range(200, 700)
-        # idx_test = range(800, 1800)
-        idx_train = range(140)
-        idx_val = range(200, 500)
-        idx_test = range(500, 1500)
     
-    elif dataset == "pubmed":
+    elif dataset == "pubmed" or dataset == "citeseer":
         transform = AddSelfLoop()  # Add self-loops
-        data = PubmedGraphDataset(transform=transform)
+        data = PubmedGraphDataset(transform=transform) if dataset == "pubmed" else CiteseerGraphDataset(transform=transform)
+
         g = data[0]  # Get the first graph object from the dataset
         features = g.ndata['feat']  # Node features
         labels = g.ndata['label']  # Node labels
@@ -84,7 +65,7 @@ def load_data(path="./data/cora/", dataset="cora"):
     adj = torch.FloatTensor(np.array(adj.todense()))
    
 
-    if dataset == "citeseer" or dataset == "cora":
+    if dataset == "cora":
         idx_train = torch.LongTensor(idx_train)
         idx_val = torch.LongTensor(idx_val)
         idx_test = torch.LongTensor(idx_test)
@@ -92,7 +73,7 @@ def load_data(path="./data/cora/", dataset="cora"):
         features = torch.FloatTensor(np.array(features.todense()))
         labels = torch.LongTensor(np.where(labels)[1])
     
-    elif dataset == "pubmed":
+    elif dataset == "pubmed" or dataset == "citeseer":
         features = torch.FloatTensor(np.array(features))
     print("all good")
 
