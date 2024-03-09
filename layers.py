5d66934a69fb390b59772bc5d6fa22a2ae534acd
YanjunZHOUU <101585425+YanjunZHOUU@users.noreply.github.com>
2024-03-09 17:34:47 +0000

    Add files via upload

--- layers.py
+++ layers.py
@@ -2,7 +2,9 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch_scatter import scatter_max
 import os
+
 
 class GraphAttentionLayer(nn.Module):
     """
@@ -21,7 +23,6 @@ class GraphAttentionLayer(nn.Module):
         nn.init.xavier_uniform_(self.W.data, gain=1.414)
         self.a = nn.Parameter(torch.empty(size=(2*out_features, 1)))
         nn.init.xavier_uniform_(self.a.data, gain=1.414)
-        #self.cnt = 0
 
         if self.skip_connection:
             self.skip_projection = nn.Parameter(torch.empty(size=(in_features, out_features)))
@@ -33,21 +34,10 @@ class GraphAttentionLayer(nn.Module):
         # Add dropout to the inputs
         h = F.dropout(h, self.dropout, training=self.training)
         Wh = torch.mm(h, self.W) # h.shape: (N, in_features), Wh.shape: (N, out_features)
-        Wh1 = torch.matmul(Wh, self.a[:self.out_features, :])
-        Wh2 = torch.matmul(Wh, self.a[self.out_features:, :])
-
-        #print(Wh1.size())
-
         # In the official repo, they add dropout after the linear projection
         Wh = F.dropout(Wh, self.dropout, training=self.training)
-
         e = self._prepare_attentional_mechanism_input(Wh)
 
-        # Applying softmax and save the attention scores
-        # The last epoch will be saved as the file is being continously overwritten
-        # I once also tried to save the attention weights in every epoch,
-        # then only preserve the one in the best epoch,
-        # but I found that takes a lot of time and might not be necessary
         Wh_att = F.softmax(e,dim=1)
         #print(Wh_att.size())
         new_directory = './attention'
@@ -58,13 +48,9 @@ class GraphAttentionLayer(nn.Module):
         zero_vec = -9e15*torch.ones_like(e)
         attention = torch.where(adj > 0, e, zero_vec)
         attention = F.softmax(attention, dim=1)
-
-        #print(attention)
-
-
         attention = F.dropout(attention, self.dropout, training=self.training)
         h_prime = torch.matmul(attention, Wh)
-        #print(h_prime)
+
         # Add skip connection
         if self.skip_connection:
             h_prime += torch.mm(h, self.skip_projection)
@@ -81,18 +67,8 @@ class GraphAttentionLayer(nn.Module):
         # e.shape (N, N)
         Wh1 = torch.matmul(Wh, self.a[:self.out_features, :])
         Wh2 = torch.matmul(Wh, self.a[self.out_features:, :])
-        #print(Wh2.size())
         # broadcast add
         e = Wh1 + Wh2.T
-        ''''
-        Wh_att = F.softmax(e, dim=1)
-        #print(Wh_att)
-
-        new_directory = './attention'
-        os.makedirs(new_directory, exist_ok=True)
-        path = f'./attention/attention.pt'
-        torch.save(Wh_att, path)
-        '''
         return self.leakyrelu(e)
 
     def __repr__(self):
@@ -173,14 +149,33 @@ class SpGraphAttentionLayer(nn.Module):
         edge_h = torch.cat((Wh[edge[0, :], :], Wh[edge[1, :], :]), dim=1).t()
         # edge: 2*D x E
 
-        edge_e = torch.exp(-self.leakyrelu(self.a.mm(edge_h).squeeze()))
+        edge_e = self.leakyrelu(self.a.mm(edge_h).squeeze())
+
+        #erowsum = self.special_spmm(edge, edge_e, torch.Size([N, N]), torch.ones(size=(N, 1), device=dv))
+
+
+        edge_e_max, _ = scatter_max(edge_e, edge[0, :])
+        edge_e = torch.exp(edge_e - edge_e_max[edge[0, :]])
         assert not torch.isnan(edge_e).any()
         # edge_e: E
 
         e_rowsum = self.special_spmm(edge, edge_e, torch.Size([N, N]), torch.ones(size=(N,1), device=dv))
         # e_rowsum: N x 1
 
+
         edge_e = F.dropout(edge_e, self.dropout, training=self.training)
+
+        e = torch.sparse_coo_tensor(edge, edge_e, torch.Size([N, N]))  # / e_rowsum
+        e = e.to_dense()
+        # e = e/erowsum
+        # print(e)
+        Wh_att = e/e_rowsum
+        # print(Wh_att.size())
+        new_directory = './attention'
+        os.makedirs(new_directory, exist_ok=True)
+        path = './attention/attention.pt'
+        torch.save(Wh_att, path)
+        #print(edge_e.size())
         # edge_e: E
 
         h_prime = self.special_spmm(edge, edge_e, torch.Size([N, N]), Wh)
@@ -232,43 +227,42 @@ class GraphAttentionLayerV2(nn.Module):
 
     def forward(self, h, adj):
         # Apply dropout to inputs
-        h = F.dropout(h, self.dropout, training=self.training)
+        h = F.dropout(h, self.dropout, training=self.training) #N * In_Feat
+        #print(h.size())
         # Project features
         Wh1 = torch.matmul(h,self.W[:self.in_features, :]) #[N, F] @ [F , F'] --> [N, F']
         Wh2 = torch.matmul(h,self.W[self.in_features:, :])
-        #print(Wh2.size())
+        #Wh1 = torch.mm(h, self.W)
         # Apply dropout to linear projections
-        Wh1 = F.dropout(Wh1, self.dropout, training=self.training) # 2708*8 or 2708*7
-        Wh2 = F.dropout(Wh2, self.dropout, training=self.training) # 2708*8 or 2708*7
-
+        Wh1 = F.dropout(Wh1, self.dropout, training=self.training)
+        Wh2 = F.dropout(Wh2 , self.dropout, training=self.training)
+        ''''
         Wh1t = Wh1.unsqueeze(2)  #2708*8*1 or 2708*7*1
         Wh2t = Wh2.T             #8*2708 or 7*2708
-        Wh2t = Wh2t.unsqueeze(0) #1*8*2708 or 1*7*2708
+        Wh2t = Wh2t.unsqueeze(0) #1*8*2708 or 1*7*2708        
 
         e = Wh1t + Wh2t  # 2708*8*2708 or 2708*7*2708
 
         e = self.leakyrelu(e) # 2708*8*2708 or 2708*7*2708
         # dot product operation: i=2708, j=8, k=2708, b=1
         e = torch.einsum('ijk,jb->ik', e, self.a)
-        #print(e.size())
-        ''''
-        # another way of implementing the dot product, same result
-        sz = Wh1.size(dim=1)
-        aa = self.a.repeat_interleave(2708, dim=0).view(1, sz, 2708).repeat(2708, 1, 1)
-        #print(aa.size())
-        e = e*aa
-        #e = torch.matmul(e, self.a) # size 2708*1                        #[N=2708, F'=64] @ [F',1] --> [N, 1]
-        #print(e.size())
-        e = e.sum(dim=1)
-        #print(e.size())
-        #e = torch.squeeze(e,dim=1)
-        #print(e.size())
         '''
-        # Applying softmax and save the attention scores
-        # The last epoch will be saved as the file is being continously overwritten
-        # I once also tried to save the attention weights in every epoch,
-        # then only preserve the one in the best epoch,
-        # but I found that takes a lot of time and might not be necessary
+        # e = Wh1 + Wh2
+        # e = self.leakyrelu(e)
+        # e = torch.matmul(e, self.a)                         #[N=2708, F'=64] @ [F',1] --> [N, 1]
+
+        Wh1t = Wh1.unsqueeze(1)  #2708*1*8
+        #Wh2t = Wh2.T             #8*2708 or 7*2708
+        Wh2t = Wh2.unsqueeze(0) #1*2708*8
+
+        e = Wh1t + Wh2t  # 2708*8*2708 or 2708*7*2708
+        #print(e.size())
+        e = self.leakyrelu(e) # 2708*8*2708 or 2708*7*2708
+        #e = torch.einsum('ijk,kb->ij', e, self.a)
+        e = torch.matmul(e,self.a)
+        e = torch.squeeze(e)
+        #print(e.size())
+
         Wh_att = F.softmax(e, dim=1)
         new_directory = './attention'
         os.makedirs(new_directory, exist_ok=True)
@@ -276,12 +270,8 @@ class GraphAttentionLayerV2(nn.Module):
         torch.save(Wh_att, path)
 
         zero_vec = -9e15*torch.ones_like(e)
-        #print(zero_vec.size())
         attention = torch.where(adj > 0, e, zero_vec)
-        #print(attention.size())
         attention = F.softmax(attention, dim=1)
-
-
         attention = F.dropout(attention, self.dropout, training=self.training)
         h_prime = torch.matmul(attention, Wh2)
 
@@ -292,6 +282,103 @@ class GraphAttentionLayerV2(nn.Module):
         if self.concat:
             return F.elu(h_prime)
         else:
+            return h_prime
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
+    
+class SpGraphAttentionLayerV2(nn.Module):
+    """
+    Sparse version GAT layer, similar to https://arxiv.org/abs/1710.10903
+    """
+
+    def __init__(self, in_features, out_features, dropout, alpha, concat=True, skip_connection=False):
+        super(SpGraphAttentionLayerV2, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.alpha = alpha
+        self.concat = concat
+        self.skip_connection = skip_connection
+        
+        self.W = nn.Parameter(torch.empty(size=(2*in_features, out_features)))
+        nn.init.xavier_normal_(self.W.data, gain=1.414)
+                
+        self.a = nn.Parameter(torch.zeros(size=(1, out_features)))
+        nn.init.xavier_normal_(self.a.data, gain=1.414)
+
+        if self.skip_connection:
+            self.skip_projection = nn.Parameter(torch.empty(size=(in_features, out_features)))
+            nn.init.xavier_uniform_(self.skip_projection.data, gain=1.414)
+
+        self.dropout = dropout
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
+        self.special_spmm = SpecialSpmm()
+
+    def forward(self, input, adj):
+        dv = 'cuda' if input.is_cuda else 'cpu'
+
+        N = input.size()[0]
+        edge = adj.nonzero().t()
+
+        # Add dropout to the inputs
+        h = F.dropout(input, self.dropout, training=self.training)
+        # Apply linear projection
+        Whi = torch.matmul(h,self.W[:self.in_features, :]) #[N, F] @ [F , F'] --> [N, F']
+        Whj = torch.matmul(h,self.W[self.in_features:, :])
+        # In the official repo, they add dropout after the linear projection
+        Whi = F.dropout(Whi, self.dropout, training=self.training)
+        Whj = F.dropout(Whj, self.dropout, training=self.training)
+        # h: N x out
+        assert not torch.isnan(Whi).any()
+        assert not torch.isnan(Whj).any()
+
+        # Self-attention on the nodes - Shared attention mechanism ( (i,j) = 1 means that there is an edge j -> i)
+        edge_h = (Whi[edge[0, :], :] + Whj[edge[1, :], :]).t()
+        # edge: D x E
+
+        edge_e = self.a.mm(self.leakyrelu(edge_h)).squeeze()
+
+
+
+        # Softmax trick to improve numerical stability
+        edge_e_max, _ = scatter_max(edge_e, edge[0, :])
+        edge_e = torch.exp(edge_e - edge_e_max[edge[0, :]])
+        assert not torch.isnan(edge_e).any()
+        # edge_e: E
+
+        e_rowsum = self.special_spmm(edge, edge_e, torch.Size([N, N]), torch.ones(size=(N,1), device=dv))
+        # e_rowsum: N x 1
+
+
+        edge_e = F.dropout(edge_e, self.dropout, training=self.training)
+        # edge_e: E
+        #erowsum = self.special_spmm(edge, edge_e, torch.Size([N, N]), torch.ones(size=(N, 1), device=dv))
+        e = torch.sparse_coo_tensor(edge, edge_e, torch.Size([N, N])) #/ e_rowsum
+        e = e.to_dense()
+        #e = e/erowsum
+        Wh_att = e/e_rowsum
+        new_directory = './attention'
+        os.makedirs(new_directory, exist_ok=True)
+        path = f'./attention/attentionv2.pt'
+        torch.save(Wh_att, path)
+
+        h_prime = self.special_spmm(edge, edge_e, torch.Size([N, N]), Whi)
+        assert not torch.isnan(h_prime).any()
+        # h_prime: N x out
+        
+        h_prime = h_prime.div(e_rowsum)
+        # h_prime: N x out
+        assert not torch.isnan(h_prime).any()
+
+        # Add skip connection
+        if self.skip_connection:
+            h_prime += torch.mm(h, self.skip_projection)
+
+        if self.concat:
+            # if this layer is not last layer,
+            return F.elu(h_prime)
+        else:
+            # if this layer is last layer,
             return h_prime
 
     def __repr__(self):
